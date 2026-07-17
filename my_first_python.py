9f94461307a134055876f607ceb0e9f2d6d59d4a
Severn <severn.peng@gmail.com>
2026-07-16 22:34:01 -0600

    commit

--- my_first_python.py
+++ my_first_python.py
@@ -1,1 +1,20 @@
-print('hello world!!')
+'''
+Author        : Severn
+Email         : severn.peng@gmail.com
+Date          : 2026-07-11 00:05:57
+LastEditors   : Severn
+LastEditTime  : 2026-07-16 21:53:00
+FilePath      : \Project1\my_first_python.py
+Description   : this is my first python file, but I don't know what to write, so I will write some comments here.
+'''
+
+def my_first_function():
+    print("Hello, World!")
+    print("This is my first Python function.")
+    return "Function executed successfully"
+
+if __name__ == "__main__":
+    result = my_first_function()
+    print(result)   
+
+
