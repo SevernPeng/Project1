42de94ce4f139949fd4919f49bf60f5b47832bc1
Severn <severn.peng@gmail.com>
2026-07-16 21:44:21 -0600

    commit agian

--- my_first_python.py
+++ my_first_python.py
@@ -3,7 +3,7 @@ Author        : Severn
 Email         : severn.peng@gmail.com
 Date          : 2026-07-11 00:05:57
 LastEditors   : Severn
-LastEditTime  : 2026-07-16 21:27:39
+LastEditTime  : 2026-07-16 21:33:41
 FilePath      : \Project1\my_first_python.py
 Description   : this is my first python file, but I don't know what to write, so I will write some comments here.
 '''
@@ -12,7 +12,7 @@ def my_first_function():
     print("Hello, World!")
     return "Function executed successfully"
 
-if __name == "__main__"":
+if __name__ == "__main__":
     result = my_first_function()
     print(result)   
 
