ec7af032bd81355d4ad58d04790ae9991f3ff0c9
Severn <severn.peng@gmail.com>
2026-07-16 21:28:41 -0600

    mommit my first python file with a header

--- my_first_python.py
+++ my_first_python.py
@@ -1,1 +1,19 @@
-print('hello world!!')
+'''
+Author        : Severn
+Email         : severn.peng@gmail.com
+Date          : 2026-07-11 00:05:57
+LastEditors   : Severn
+LastEditTime  : 2026-07-16 21:27:39
+FilePath      : \Project1\my_first_python.py
+Description   : this is my first python file, but I don't know what to write, so I will write some comments here.
+'''
+
+def my_first_function():
+    print("Hello, World!")
+    return "Function executed successfully"
+
+if __name == "__main__"":
+    result = my_first_function()
+    print(result)   
+
+
