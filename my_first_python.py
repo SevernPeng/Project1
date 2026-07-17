ed932b4d0d0f8481bed2e2042257db3bfa0142e5
Severn <severn.peng@gmail.com>
2026-07-16 23:02:52 -0600

    delete a line

--- my_first_python.py
+++ my_first_python.py
@@ -3,14 +3,13 @@ Author        : Severn
 Email         : severn.peng@gmail.com
 Date          : 2026-07-11 00:05:57
 LastEditors   : Severn
-LastEditTime  : 2026-07-16 21:53:00
+LastEditTime  : 2026-07-16 22:35:48
 FilePath      : \Project1\my_first_python.py
 Description   : this is my first python file, but I don't know what to write, so I will write some comments here.
 '''
 
 def my_first_function():
     print("Hello, World!")
-    print("This is my first Python function.")
     return "Function executed successfully"
 
 if __name__ == "__main__":
