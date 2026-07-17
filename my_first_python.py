e2688260811e4216b8e6691157839902f3b1a0ad
Severn <severn.peng@gmail.com>
2026-07-16 21:53:35 -0600

    added a line print

--- my_first_python.py
+++ my_first_python.py
@@ -3,13 +3,14 @@ Author        : Severn
 Email         : severn.peng@gmail.com
 Date          : 2026-07-11 00:05:57
 LastEditors   : Severn
-LastEditTime  : 2026-07-16 21:33:41
+LastEditTime  : 2026-07-16 21:53:00
 FilePath      : \Project1\my_first_python.py
 Description   : this is my first python file, but I don't know what to write, so I will write some comments here.
 '''
 
 def my_first_function():
     print("Hello, World!")
+    print("This is my first Python function.")
     return "Function executed successfully"
 
 if __name__ == "__main__":
